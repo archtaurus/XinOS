8e676b0438526cc09feb9d9745172b5e0733af12
Zhao Xin <pythonchallenge@qq.com>
2017-04-29 18:13:52 +0800

    update

--- source/main.c
+++ source/main.c
@@ -3,12 +3,12 @@
 #include "xinos.h"
 
 int main(void) {
-    string str = "strlen(\"os system\") = ";
+    string str = "strlen(\"system\") = ";
 
     clear();
     mvaddstr("Welcome!", 10, 30, 0x0C);
     mvaddstr("This is the XinOS...", 12, 30, 0x09);
     mvaddstr(str, 14, 30, 0x0A);
-    mvaddch(strlen("os system") + '0', 14, 52, 0);
+    mvaddch(strlen("system") + '0', 14, 49, 0);
     return 0;
 }
