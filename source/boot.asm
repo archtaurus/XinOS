e19012922b8999f37394e8a513a9f799b1b596f2
Zhao Xin <pythonchallenge@qq.com>
2017-05-02 00:41:33 +0800

    update

--- source/boot.asm
+++ source/boot.asm
@@ -12,7 +12,7 @@
 ; 32-bit boundary. The signature is in its own section so the header can be
 ; forced to be within the first 8 KiB of the kernel file.
 [section .multiboot_header]
-align  4
+align 4
 	dd multiboot_magic
 	dd multiboot_flags
 	dd multiboot_checksum
@@ -24,19 +24,19 @@ align  4
 [section .text]
 global start:function (end - start)
 start:
-    extern kmain
-    call kmain  ; call c kmain function
+	extern kmain
+	call kmain  ; call c kmain function
 
-    ; If the system has nothing more to do, put the computer into an
-    ; infinite loop. To do that:
-    ; 1) Disable interrupts with cli (clear interrupt enable in eflags).
-    ;    They are already disabled by the bootloader, so this is not needed.
-    ;    Mind that you might later enable interrupts and return from
-    ;    kernel_main (which is sort of nonsensical to do).
-    ; 2) Wait for the next interrupt to arrive with hlt (halt instruction).
-    ;    Since they are disabled, this will lock up the computer.
-    ; 3) Jump to the hlt instruction if it ever wakes up due to a
-    ;    non-maskable interrupt occurring or due to system management mode.
+	; If the system has nothing more to do, put the computer into an
+	; infinite loop. To do that:
+	; 1) Disable interrupts with cli (clear interrupt enable in eflags).
+	;    They are already disabled by the bootloader, so this is not needed.
+	;    Mind that you might later enable interrupts and return from
+	;    kernel_main (which is sort of nonsensical to do).
+	; 2) Wait for the next interrupt to arrive with hlt (halt instruction).
+	;    Since they are disabled, this will lock up the computer.
+	; 3) Jump to the hlt instruction if it ever wakes up due to a
+	;    non-maskable interrupt occurring or due to system management mode.
 	cli 		; clear interrupts
 halt:
 	hlt 		; halt the CPU
