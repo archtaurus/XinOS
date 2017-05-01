6d1d92b170eb535db76ed279105d94564fd209ea
Zhao Xin <pythonchallenge@qq.com>
2017-05-02 00:35:58 +0800

    update

--- include/screen.h
+++ include/screen.h
@@ -3,6 +3,25 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
+typedef enum {
+    COLOR_BLACK         = 0x00,
+    COLOR_BLUE          = 0x01,
+    COLOR_GREEN         = 0x02,
+    COLOR_CYAN          = 0x03,
+    COLOR_RED           = 0x04,
+    COLOR_MAGENTA       = 0x05,
+    COLOR_BROWN         = 0x06,
+    COLOR_LIGHT_GRAY    = 0x07,
+    COLOR_DARK_GRAY     = 0x08,
+    COLOR_LIGHT_BLUE    = 0x09,
+    COLOR_LIGHT_GREEN   = 0x0A,
+    COLOR_LIGHT_CYAN    = 0x0B,
+    COLOR_LIGHT_RED     = 0x0C,
+    COLOR_LIGHT_MAGENTA = 0x0D,
+    COLOR_YELLOW        = 0x0E,
+    COLOR_WHITE         = 0x0F,
+} color_t;
+
 void cls(void);
 uint16_t getchidx(uint16_t, uint16_t);
 void putcat(char, uint16_t, uint16_t, uint8_t);
--- source/screen.c
+++ source/screen.c
@@ -4,39 +4,14 @@
 #include "string.h"
 #include "screen.h"
 
-// Bit 76543210
-//     ||||||||
-//     |||||^^^-fore colour
-//     ||||^----fore colour bright bit
-//     |^^^-----back colour
-//     ^--------back colour bright bit OR enables blinking Text
-typedef enum {
-    COLOR_BLACK         = 0x00,
-    COLOR_BLUE          = 0x01,
-    COLOR_GREEN         = 0x02,
-    COLOR_CYAN          = 0x03,
-    COLOR_RED           = 0x04,
-    COLOR_MAGENTA       = 0x05,
-    COLOR_BROWN         = 0x06,
-    COLOR_LIGHT_GRAY    = 0x07,
-    COLOR_DARK_GRAY     = 0x08,
-    COLOR_LIGHT_BLUE    = 0x09,
-    COLOR_LIGHT_GREEN   = 0x0A,
-    COLOR_LIGHT_CYAN    = 0x0B,
-    COLOR_LIGHT_RED     = 0x0C,
-    COLOR_LIGHT_MAGENTA = 0x0D,
-    COLOR_YELLOW        = 0x0E,
-    COLOR_WHITE         = 0x0F,
-} color_t;
-
 // http://wiki.osdev.org/Bare_Bones#Writing_a_kernel_in_C
 //
 static inline uint8_t entry_color(color_t fgcolor, color_t bgcolor) {
     return fgcolor | bgcolor << 4;
 }
 
-static inline uint16_t entry(uint8_t ch, uint8_t color) {
-    return (uint16_t) ch | (uint16_t) color << 8;
+static inline uint16_t entry(uint8_t ch, uint8_t entry_color) {
+    return (uint16_t) ch | (uint16_t) entry_color << 8;
 }
 
 uint16_t screenW = 80;     // 屏幕宽
